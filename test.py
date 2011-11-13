908de545fada8508b40090c98741fa5db2609a72
Lucas Doyle <lucas@lucasdoyle.com>
2011-11-13 12:56:34 -0500

    updated tests

--- test.py
+++ test.py
@@ -2,17 +2,23 @@ from objects import robot
 import json, sys
 
 
-def as_robot(dict):
-    return robot(dict)
-
-r = json.loads(open('robots/sample.json').read(), object_hook=as_robot)
+def create_robot(filename):
+    r = json.loads(open(filename).read(), object_hook=lambda d: robot(d))
+    return r
 
 #r.forwardkin()
 
 #sys.exit()
 
-for i in range(5):
-    r.timestep()
-    r.forwardkin()
-    #print r.R0T
-    print r.P0T
+r = create_robot('robots/sample.json')
+#print 'N = ' + str(r.N)
+#print 'q1 = ' + str(r.q1)
+#print 'P01 = ' + str(r.P01)
+#print 'P12 = ' + repr(r.P12)
+#print 'R23 = ' + str(r.R23)
+
+
+for i in range(20):
+  r.forwardkin()
+  r.timestep()
+  print 'R23 = ' + str(r.R23)
